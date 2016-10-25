22cd0dd962b7261340e594a1ae7138c2fa6c4c7e
Benoit Sagols <benoit.sagols@gmail.com>
2016-10-25 16:30:48 +0700

    Fix dev

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -5,15 +5,15 @@ return [
         'index' => ':resource_plural',
         'create' => 'Add new :resource_singular',
         'edit' => 'Edit :resource_singular',
-    ]
+    ],
     'errors' => [
         'store' => '<strong>An error occurred</strong>. Please review your input.',
-        'update' => '<strong>An error occurred</strong>. Please review your input.'
+        'update' => '<strong>An error occurred</strong>. Please review your input.',
     ],
     'success' => [
         'store' => 'New :resource has been successfully created.',
-        'update' => ':resource has been successfully updated.'
-        'destroy' => ':resource has been successfully deleted.'
+        'update' => ':resource has been successfully updated.',
+        'destroy' => ':resource has been successfully deleted.',
     ],
     'no_resource_found' => 'No :resource_singular found.',
 ];
