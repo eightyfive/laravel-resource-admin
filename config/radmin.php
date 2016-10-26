be4906102afa881c0a444287f5de5606813c0228
Benoit Sagols <benoit.sagols@gmail.com>
2016-10-26 15:57:48 +0700

    added config

---
+++ config/radmin.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    'namespaces' => [
+        'controllers' => 'App\\Http\\Controllers\\',
+        'forms' => 'App\\Forms\\',
+        'models' => 'App\\',
+    ]
+
+    'styles' => [
+        'https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.4/semantic.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.4/components/checkbox.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.4/components/dropdown.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.4/components/list.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.4/components/modal.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.4/components/tab.min.css',
+    ],
+
+    'scripts' => [
+        'https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.4/semantic.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.4/components/checkbox.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.4/components/dropdown.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.4/components/modal.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.4/components/tab.min.js',
+    ],
+
+    'css' => [
+        'form' => 'ui form',
+        'btn_primary' => 'ui primary button',
+        'btn_secondary' => 'ui basic button',
+    ],
+];
